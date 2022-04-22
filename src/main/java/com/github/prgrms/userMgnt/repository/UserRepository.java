654997608955cc92b467f3a4dff471fc0d79285c
SONGYOUNGWOO <jeroso@ldfactory.com>
2022-04-22 18:00:27 +0900

    repository jdbc 생성 및 override

--- src/main/java/com/github/prgrms/userMgnt/repository/UserRepository.java
+++ src/main/java/com/github/prgrms/userMgnt/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.github.prgrms.userMgnt.repository;
 
+import com.github.prgrms.userMgnt.model.User;
+
+import java.util.List;
+
 public interface UserRepository {
+
+    public User findById(Long id);
+
+    public void add(User user);
+
+    public List<User> findAll();
 }
--- src/main/java/com/github/prgrms/userMgnt/repository/UserRepositoryImpl.java
+++ src/main/java/com/github/prgrms/userMgnt/repository/UserRepositoryImpl.java
@@ -1,4 +1,28 @@
 package com.github.prgrms.userMgnt.repository;
 
-public class UserRepositoryImpl {
+import com.github.prgrms.userMgnt.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserRepositoryImpl implements UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepositoryImpl(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
 }
